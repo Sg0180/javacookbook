5a4db3ff984aa043fdff4d69ca94dab5ad8f89dd
sameer gopali <sg0180@uah.edu>
2023-07-02 13:19:06 -0400

    Chapter 1

--- src/main/java/book/Main.java
+++ src/main/java/book/Main.java
@@ -1,4 +1,11 @@
 package book;
 
+import java.util.Date;
+
 public class Main {
+    public static void main(String [] args){
+        System.out.println("hello world!!!");
+    }
+
+    Date date  =  new Date(23,07,02);
 }
---
+++ src/test/java/book/MainTest.java
@@ -0,0 +1,17 @@
+package book;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void test(){
+        assertEquals(1+1,3);
+    }
+
+
+}
