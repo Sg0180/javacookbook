c2d6a05ae05b92654f09a1a4e43961b4b9595e01
sameer gopali <sg0180@uah.edu>
2023-07-02 14:53:37 -0400

    Chapter 2

--- src/main/java/book/Main.java
+++ src/main/java/book/Main.java
@@ -1,4 +1,9 @@
 package book;
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println("System.getenv(\"PATH\") = " + System.getenv("PATH"));
+        System.getProperties().forEach((k,v)-> System.out.println(k+"->"+v));
+        System.out.println(System.getProperty("os.arch"));
+    }
 }
