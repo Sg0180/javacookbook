5397b838fd34c8e971ced71d999eee410b771850
sameer gopali <sg0180@uah.edu>
2023-07-03 10:14:51 -0400

    Chapter 7

--- src/main/java/book/Main.java
+++ src/main/java/book/Main.java
@@ -1,4 +1,41 @@
 package book;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args){
+        /*
+        List, Set, Map, Queue  -- four data structures
+        Sequences  -- List, Set
+            - List : preserves order, allows duplicates
+        Map - Key, value
+
+        List - Arraylist, Linkedlist, vector
+        Set  - Hashset, Tree set
+        ,
+
+        Collections  Queue, List, Set,
+        MAp
+        */
+
+        List<String> myList = new ArrayList<>();
+        // var myList =  new ArrayList<String>();
+        myList.add("Hello");
+        myList.add("World");
+        myList.add("of");
+        myList.add("Java");
+        System.out.println(myList);
+        // to array
+        System.out.println(Arrays.toString(myList.toArray()));
+        // arrays to list
+        //List<String> firstname =  List.of("Robin", "Bruce","Ted");
+        List <String> firstname  =  Arrays.asList("abc","def");
+        var blist  = List.of("abc", "def","ghsi");
+        System.out.println(blist);
+
+
+
+    }
 }
